e4a497298f803c566bd18d5bacd2cb9a389a5011
Valentin Marmin <valentin.marmin@gmail.com>
2020-01-03 19:09:29 -0500

    End of chapter 2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,14 @@
 from app import app
+from flask import render_template
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!!"
+    user = {"username": "Valentin"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
+    # return render_template("index.html", user=user)
