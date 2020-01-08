34307898b97e9de3811c00537f294898d6492e53
Valentin Marmin <vmarmin@xiplink.com>
2020-01-08 15:37:51 -0500

    Adding user login

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from app import models, routes  # noqa
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-from app import routes, models  # noqa
+login = LoginManager(app)
