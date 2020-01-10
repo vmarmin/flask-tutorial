1655e6f3f76261e371ffe9860c20288c9c214ace
Valentin Marmin <vmarmin@xiplink.com>
2020-01-10 09:49:18 -0500

    Fixing isort+black and adding black configuration file

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,8 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import (BooleanField, PasswordField, StringField, SubmitField,
-                     TextAreaField)
-from wtforms.validators import (DataRequired, Email, EqualTo, Length,
-                                ValidationError)
+from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 
 from app.models import User
 
