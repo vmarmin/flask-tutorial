9f458d03b5257e1b7e604083faf5a4a1e08b912f
Valentin Marmin <vmarmin@xiplink.com>
2020-01-14 17:36:29 -0500

    Adding email notifications and logs

--- microblog/config.py
+++ microblog/config.py
@@ -4,8 +4,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    # App key
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+
+    # Database settings
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Email settings
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = ["valentin.marmin@gmail.com"]
