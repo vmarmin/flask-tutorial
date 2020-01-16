16b7a117ea0e16bfc112c8a9b913c317b90f060c
Valentin Marmin <vmarmin@xiplink.com>
2020-01-16 17:27:29 -0500

    Adding blog post query + user page link.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,19 +5,24 @@ from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
 
 from app import app, db
-from app.forms import EditProfileForm, LoginForm, RegistrationForm
-from app.models import User
+from app.forms import EditProfileForm, LoginForm, PostForm, RegistrationForm
+from app.models import Post, User
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 @login_required
 def index():
-    posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
-    ]
-    return render_template("index.html", title="Home", posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post is now live!")
+        return redirect(url_for("index"))
+    # Get post list
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title="Home", posts=posts, form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -127,3 +132,10 @@ def unfollow(username):
     db.session.commit()
     flash("You are not following {}".format(username))
     return redirect(url_for("user", username=username))
+
+
+@app.route("/explore")
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template("index.html", title="Explore", posts=posts)
--- readme.md
+++ readme.md
@@ -10,4 +10,4 @@ Miguel Grinberg.
 
 ## Resources
 
-https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+[https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world)
